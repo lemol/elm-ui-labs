831f3239192e71d1dd4f1b3f125055f0a43d6ed4
Leza Lutonda (lemol) <lemol-c@hotmail.com>
2019-10-16 01:45:59 +0100

    Doing elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,30 @@
 module Main exposing (main)
 
-import App as App
 import Browser
+import Css as Css
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Events exposing (..)
 import Element.Font as Font
 import Element.Input as Input
-import Html exposing (Html, div)
+import Html exposing (Html, div, img)
+import Html.Attributes as HA
+import Html.Styled as Styled
+import Html.Styled.Attributes as StyledA
 
 
 type alias Model =
-    { app : App.Model
-    }
+    Int
 
 
 type Msg
-    = AppMsg App.Msg
+    = Nop
 
 
 init : Model
 init =
-    { app = App.defaultModel }
+    0
 
 
 main =
@@ -40,38 +42,89 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    layout [] <|
+        column
+            [ height fill, width fill ]
+            [ header
+            , body
+            , footer
+            ]
+
+
+header : Element msg
+header =
     let
-        app =
-            App.view model.app
+        url =
+            Just "https://lh3.googleusercontent.com/-GV9ojxr4eVk/AAAAAAAAAAI/AAAAAAAAALI/syV20LUzuvI/photo.jpg"
     in
-    div []
-        [ ui
-        , Html.map AppMsg app.topAppBar
-        , ui
+    row
+        [ height <| px 48
+        , width fill
+        , Background.color <| rgb255 92 99 11
+        ]
+        [ el
+            [ alignRight, padding 4 ]
+            (avatar url "LM")
         ]
 
 
-channelPanel : Element msg
-channelPanel =
-    column
-        [ height fill
-        , width <| fillPortion 1
+avatar : Maybe String -> String -> Element msg
+avatar src alt =
+    case src of
+        Just srcUrl ->
+            html <|
+                Styled.toUnstyled <|
+                    Styled.img
+                        [ StyledA.src srcUrl
+                        , StyledA.alt alt
+                        , StyledA.css
+                            [ Css.width (Css.px 32)
+                            , Css.height (Css.px 32)
+                            , Css.borderRadius (Css.pct 50)
+                            ]
+                        ]
+                        []
+
+        Nothing ->
+            el
+                [ width <| px 32
+                , height <| px 32
+                , Border.rounded 50
+                , Background.color <| rgb255 15 69 11
+                ]
+            <|
+                el
+                    [ centerX, centerY, Font.size 16 ]
+                    (text alt)
+
+
+body : Element msg
+body =
+    row
+        [ centerY
+        , height fill
+        , width fill
+        , padding 10
         , Background.color <| rgb255 92 99 118
         , Font.color <| rgb255 255 255 255
         ]
-        [ text "channels" ]
+        [ searchBox "ok" Debug.todo ]
 
 
-chatPanel : Element msg
-chatPanel =
-    column [ height fill, width <| fillPortion 5 ]
-        [ text "chat2" ]
+searchBox : String -> (String -> msg) -> Element msg
+searchBox value onChange =
+    Input.text
+        [ width <| fillPortion 4
+        , Border.glow (rgb255 200 200 200) 4
+        , Border.rounded 10
+        , Border.solid
+        , Border.color (rgb255 180 180 180)
+        ]
+        { text = value, onChange = onChange, placeholder = Nothing, label = Input.labelHidden "" }
 
 
-ui : Html msg
-ui =
-    layout [] <|
-        row [ height fill, width fill ]
-            [ channelPanel
-            , chatPanel
-            ]
+footer : Element msg
+footer =
+    row
+        [ width fill, height <| px 20, Background.color <| rgb255 55 84 10 ]
+        [ text "(c) 2019" ]
