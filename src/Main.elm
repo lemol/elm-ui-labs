9cfb55bc47d65f608587d7cc28fda9564932fa12
Leza Morais Lutonda <lemol-c@hotmail.com>
2019-10-18 00:07:59 +0000

    working on login design

--- src/Main.elm
+++ src/Main.elm
@@ -15,16 +15,17 @@ import Html.Styled.Attributes as StyledA
 
 
 type alias Model =
-    Int
+    { hoverBox : Bool }
 
 
 type Msg
-    = Nop
+    = NoOp
+    | BoxHovered Bool
 
 
 init : Model
 init =
-    0
+    { hoverBox = False }
 
 
 main =
@@ -37,11 +38,154 @@ main =
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        BoxHovered state ->
+            { model | hoverBox = state }
+
+        NoOp ->
+            model
 
 
 view : Model -> Html Msg
 view model =
+    layout [] <|
+        row
+            [ height fill
+            , width fill
+            , Background.color <| rgb255 240 240 240
+            ]
+            [ box model
+            ]
+
+
+box : Model -> Element Msg
+box model =
+    el
+        [ centerX
+        , centerY
+        , padding 24
+        , width (fill |> maximum 505)
+        , baseFont
+        , Background.color <| rgb255 255 255 255
+        , Border.rounded 16
+        , Border.glow (rgba255 0 0 0 0.3) 6
+        , mouseOver [ Border.glow (rgba255 0 0 0 0.3) 12 ]
+        ]
+    <|
+        column
+            [ width fill ]
+            [ boxHeader
+            , boxBody
+            ]
+
+
+baseFont =
+    Font.family <|
+        List.map Font.typeface
+            [ "-apple-system"
+            , "BlinkMacSystemFont"
+            , "Segoe UI"
+            , "Roboto"
+            , "Helvetica Neue"
+            , "Arial"
+            , "sans-serif"
+            , "Apple Color Emoji"
+            , "Segoe UI Emoji"
+            , "Segoe UI Symbol"
+            ]
+
+
+boxHeader : Element msg
+boxHeader =
+    column
+        [ padding 16, spacing 8 ]
+        [ el
+            [ Font.size 12
+            ]
+            (text "CRIE A SUA CONTA")
+        , paragraph
+            [ Font.size 14
+            ]
+            [ text "Ou faça o login com a sua "
+            , link
+                [ Font.color <| rgb255 56 151 240
+                ]
+                { url = "#", label = text "conta já existente" }
+            ]
+        ]
+
+
+boxBody : Element msg
+boxBody =
+    column
+        [ width fill
+        , padding 16
+        , spacing 16
+        , Font.size 16
+        ]
+        [ formRow
+            [ commonInput [] 1 "Nome completo" ]
+        , formRow
+            [ commonInput [] 1 "Code"
+            , commonInput [] 3 "Telefone"
+            ]
+        , formRow
+            [ commonNewPassword [] 1 "Palavra passe"
+            ]
+        , paragraph []
+            [ text "Ao criar uma conta aceita "
+            , link [ Font.color <| rgb255 56 151 240 ] { url = "#", label = text "os nossos termos e condições" }
+            ]
+        , row []
+            [ Input.button [] { onPress = Nothing, label = text "CRIAR CONTA" } ]
+        ]
+
+
+formRow children =
+    row
+        [ spacing 8
+        , width fill
+        ]
+        children
+
+
+commonControlsAttrs : List (Attribute msg)
+commonControlsAttrs =
+    [ spacing 12
+    , Border.rounded 100
+    , Border.width 0
+    , Background.color <| rgb255 241 243 244
+    ]
+
+
+commonInput : List (Attribute msg) -> Int -> String -> Element msg
+commonInput attrs portion label =
+    Input.text
+        (commonControlsAttrs
+            ++ [ width <| fillPortion portion
+               ]
+            ++ attrs
+        )
+        { onChange = Debug.todo, text = "", placeholder = Just <| Input.placeholder [] (text label), label = Input.labelHidden label }
+
+
+commonNewPassword : List (Attribute msg) -> Int -> String -> Element msg
+commonNewPassword attrs portion label =
+    Input.newPassword
+        (commonControlsAttrs
+            ++ [ width <| fillPortion portion
+               ]
+            ++ attrs
+        )
+        { onChange = Debug.todo, text = "", placeholder = Just <| Input.placeholder [] (text label), label = Input.labelHidden label, show = False }
+
+
+
+----------------------
+
+
+view2 : Model -> Html Msg
+view2 model =
     layout [] <|
         column
             [ height fill, width fill ]
